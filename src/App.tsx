591b691b55aaaf3a233af58be8238f483be046b4
Michael Campbell <michael.campbell14@gmail.com>
2024-02-07 15:54:23 -0500

    installing and setting up tailwind css with vite

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <div>
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      {/* <div>
         <img src={bearCuteLove} className='logo react' alt='React logo' />
         <img src={kissingBear} className='logo' alt='Vite logo' />
       </div>
       <h1>Will you be my valentine?</h1>
       <button>Yes</button>
-      <button>No</button>
-    </>
+      <button>No</button> */}
+    </div>
   );
 }
 
