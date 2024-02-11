4e2ab3980db1d73e91a9205da2ed80cecdf151e5
Michael Campbell <michael.campbell14@gmail.com>
2024-02-10 19:06:43 -0500

    adding most of the logic for the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
+import { useState } from 'react';
 import bearCuteLove from './assets/cute-love.gif';
 import kissingBear from './assets/bear-kisses.gif';
-// import './App.css';
 
 function App() {
+  let [yes, setYes] = useState(false);
+  let [noNum, setNoNum] = useState(0);
+
+  const noArray = [
+    'No',
+    'Are you sure?',
+    'Really sure?',
+    'Think again!',
+    'Last chance!',
+    'Surely not?',
+    'You might regret this!',
+    'Give it another thought!',
+    'Are you absoluty serious?',
+    'This could be a mistake!',
+    'Have a heart',
+    "Don't be so cold",
+    'Change of heart?',
+    "Wouldn't you reconsider",
+    "Is that You're final anwser?",
+    "You're breaking my heart :(",
+  ];
+
+  const noButt = () => {
+    if (noNum <= 14) {
+      let test = noNum;
+      test++;
+      setNoNum(test);
+      console.log(noNum);
+    } else if (noNum > 14) {
+      setNoNum(1);
+    }
+  };
+
+  const yesButt = () => {
+    setYes(true);
+  };
+
   return (
     <section className=' h-screen bg-red-50 flex items-center justify-center flex-col text-center text-3xl'>
       <div className='flex items-center justify-center flex-col child:p-4'>
         <div>
-          <img src={bearCuteLove} alt='React logo' />
-          <img className='hidden' src={kissingBear} alt='Vite logo' />
+          {yes ? (
+            <img src={kissingBear} alt='kisisng bears' />
+          ) : (
+            <img src={bearCuteLove} alt='bear with rose and hearts' />
+          )}
         </div>
-        <h1>Will you be my Valentine?</h1>
-        <div className='w-full flex justify-around'>
-          <button className='heart'>
-            <p>Yes</p>
-          </button>
-          <button className='heart'>
-            <p>No</p>
-          </button>
-        </div>
+        {!yes ? <h1>Will you be my Valentine?</h1> : <h1>Luv U 🥰</h1>}
+        {!yes ? (
+          <div className='w-full flex justify-around'>
+            <button className='heart' onClick={yesButt}>
+              <p>Yes</p>
+            </button>
+            {noNum === 0 && !yes ? (
+              <button onClick={noButt} className='heart'>
+                <p>No</p>
+              </button>
+            ) : (
+              <button className='bg-gray-400 px-4 pt-0' onClick={noButt}>
+                {noArray[noNum]}
+              </button>
+            )}
+          </div>
+        ) : (
+          <p></p>
+        )}
       </div>
     </section>
   );
